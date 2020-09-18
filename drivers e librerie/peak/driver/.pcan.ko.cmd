9da75ba4f6e344a3a6b7d67351e39e971fa128c1
Filippo Guarda <filippoguarda@pop-os.localdomain>
2020-09-18 22:12:37 +0200

    added content

---
+++ drivers e librerie/peak/driver/.pcan.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/filippoguarda/peak/driver/pcan.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/filippoguarda/peak/driver/pcan.ko /home/filippoguarda/peak/driver/pcan.o /home/filippoguarda/peak/driver/pcan.mod.o ;  true
